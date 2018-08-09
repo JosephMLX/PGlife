445702b96c7c6ef4542ca816ffc6a9e43edc3578
JosephMLX <menglingxu95@gmail.com>
2018-08-09 14:12:26 +1000

    week 3

---
+++ COMP9041/lab03/file_sizes.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+S="Small files: "
+M="Medium-sized files: "
+L="Large files: "
+
+for file in *
+do
+	lines=`wc -l < $file`
+	if test $lines -lt 10
+	then
+		S+="$file "
+	elif test $lines -lt 100
+	then
+		M+="$file "
+	else
+		L+="$file "
+	fi
+done
+		
+echo $S
+echo $M
+echo $L
