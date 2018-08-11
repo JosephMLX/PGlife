3e3658ba569dfb0245512730828c270f7022b10c
JosephMLX <menglingxu95@gmail.com>
2018-08-11 21:05:45 +1000

    week 3

---
+++ COMP9024/Problem Set/Week_2/question3_Heap's_algorithm.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void swap (char *x, char *y)
+{
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void heappermute(char v[], int n) {
+	int i;
+	char temp;
+	for (i = 0; i < n; i++) {
+    	heappermute(v, n-1);
+        if (n % 2 == 1) {
+        	swap(&v[0], &v[n-1]);
+	    }
+        else {
+            swap(&v[i], &v[n-1]);
+        }
+	}
+}
+ 
+int main()
+{
+    char catdog[] = {'a','c','d','g','o','t'};
+    int  i = 6;
+    heappermute(catdog, i);
+    return 0;
+}
