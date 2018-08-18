652a0db1c13e9d80ccbf6b2e868f4215303f5400
JosephMLX <menglingxu95@gmail.com>
2018-08-18 15:59:12 +1000

    week 4

--- COMP9041/lab04/tag_music.sh
+++ COMP9041/lab04/tag_music.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-for file in $1
+for folder in "$@"
 do
-	echo `ls -l $file`
+	for file in "$folder"/*.mp3 
+	do
+		if test -e "$file"
+		then
+			id3 -t "`ls -l "$file" | cut -d ' ' -f10- | cut -d '-' -f2 | cut -c2-`" "$file" 1>/dev/null
+			id3 -a "`ls -l "$file" | cut -d ' ' -f10- | cut -d '-' -f3 | sed s/'.mp3'//g | cut -c2-`" "$file" 1>/dev/null
+			id3 -A "`ls -l "$file" | cut -d '/' -f2`" "$file" 1>/dev/null
+			id3 -y "`ls -l "$file" | cut -d '/' -f2 | cut -d ',' -f2 | cut -c2-`, Genre: Unknown (255)" "$file" 1>/dev/null
+			id3 -T "`ls -l "$file" | cut -d ',' -f2 | cut -d '-' -f1 | cut -d '/' -f2- | sed -e 's/\///g'`" "$file" 1>/dev/null
+		#echo "`ls -l "$file" | cut -d ',' -f2 | cut -d '-' -f1 | cut -d '/' -f2- | sed -e 's/\///g'`"
+		fi
+	done
 done
+
