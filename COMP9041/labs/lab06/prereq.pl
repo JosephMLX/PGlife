f6e22eafc08d5e02dff04e710056fa034acac3b3
JosephMLX <menglingxu95@gmail.com>
2018-08-30 01:39:29 +1000

    week 6

--- COMP9041/labs/lab06/prereq.pl
+++ COMP9041/labs/lab06/prereq.pl
@@ -5,7 +5,6 @@ $ug = "undergraduate";
 $pg = "postgraduate";
 $url_ug = "http://www.handbook.unsw.edu.au/$ug/courses/2018/$course.html";
 $url_pg = "http://www.handbook.unsw.edu.au/$pg/courses/2018/$course.html";
-print "$url_pg", "\n";
 open F, "wget -q -O- $url_ug|" or die;
 # if it is a pg course, F is null, then scrap from pg website;
 if (! <F>) {
@@ -14,7 +13,16 @@ if (! <F>) {
 # open O, '>', "output.txt";
 while ($line = <F>) {
 	chomp $line;
-    print $line;
+	if ($line =~ /Prereq/) {
+		$line =~ s/Excluded.*//;
+		$line =~ s/.*<p>Prerequisite: //;
+		$line =~ s/<\/p><p><strong>//;
+		@courses = split ("or", $line);
+		@courses = sort(@courses);
+		foreach $i (@courses) {
+			$i =~ s/ //;
+			print "$i", "\n";
+		}
+	}
 }
-
 # close O;
