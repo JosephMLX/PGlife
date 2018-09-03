eb53682a6f28f7ac4428ff26a21b628a2d1c5421
JosephMLX <menglingxu95@gmail.com>
2018-09-04 00:53:14 +1000

    week 7

--- COMP9041/labs/lab07/identify_artist.pl
+++ COMP9041/labs/lab07/identify_artist.pl
@@ -1,34 +1,75 @@
 #!/usr/bin/perl -w
 
-my $total_count = 0;
-my $word_count = 0;
-my $defined_word = $ARGV[0];
-
 foreach $file (glob "lyrics/*.txt") {
 	my $artist = $file;
+	my $total_count = 0;
 	$artist =~ s/.*\///;		# remove all letters between name
 	$artist =~ s/\..*//;		# remove all letters after name
-	$artist =~ s/_/ /g;			# replace all underline with space	
+	$artist =~ s/_/ /g;			# replace all underline with space
 	open FILE, '<', "$file" or die;
-	foreach $line (<FILE>) {
+	foreach my $line (<FILE>) {
 		$line =~ s/[^ a-zA-Z]/ /g;
 		$line = lc $line;
 		@words = split(' ', $line);
 		$total_count += $#words + 1;
+	}
+	$freq{$artist}[0] = $total_count;
+	$freq{$artist}[1] = 0;
+}
+
+if ($ARGV[0] eq "-d") {
+	single_file();
+}
+
+sub single_file {
+	my $file = $ARGV[1];
+	open FILE, '<', "$file" or die;
+	foreach $line (<FILE>) {
+		$line =~ s/[^ a-zA-Z]/ /g;    # replace all non-alphabetic characters and keep space
+		$line = lc $line;
+		@words = split(' ', $line);
 		foreach $word (@words) {
+			count_word_all($word);
+		}		
+	}
+}
+
+sub count_word_all {
+	my @defined_word = @_;
+	foreach $key (sort keys %freq) {
+		my $filename = $key;
+		$filename =~ s/ /_/g;
+		$filename = "lyrics/$filename.txt";
+		$freq{$key}[1] += log((count_word_single(@defined_word, $filename)+1)/$freq{$key}[0]);
+	}
+}	
+
+sub count_word_single {
+	my ($defined_word, $filename) = @_;
+	my $word_count = 0;
+	open FILE, '<', "$filename" or die;
+	foreach my $line (<FILE>) {
+		$line =~ s/[^ a-zA-Z]/ /g;    # replace all non-alphabetic characters and keep space
+		$line = lc $line;
+		my @words = split(' ', $line);
+		foreach my $word (@words) {
 			if ($word eq $defined_word) {
 				$word_count += 1;	
-			}	
-		}
+			}
+		}	
 	}
-	$freq{$artist}[0] = $word_count;
-	$freq{$artist}[1] = $total_count;
-	$freq{$artist}[2] = log(($word_count+1) / $total_count);
-	$word_count = 0;
-	$total_count = 0;
-	#close;
-	#print "$artist\n";
+	return $word_count;
 }
-foreach $key (sort keys %freq) {
-	print sprintf("log((%d+1)/%6d) =  %.4f %s\n", $freq{$key}[0], $freq{$key}[1], $freq{$key}[2], $key);
+
+foreach my $key (sort keys %freq) {
+	$hash{$freq{$key}[1]} = $key;
+}
+
+if ($ARGV[0] eq "-d") {
+	foreach my $key (sort keys %hash) {
+		print sprintf("%s: log_probability of %.1f for %s\n", $ARGV[1], $key, $hash{$key});
+	}
+	$possibility = (sort keys %hash)[0];
+	$singer = $hash{$possibility};
+	print sprintf("%s most resembles the work of %s (log-probability=%.1f)\n", $ARGV[1], $singer, $possibility);
 }
