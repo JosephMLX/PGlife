452baee9fe25a44d359f4bfb9564df5e16e50237
Lingxu Meng <mlx@Lingxus-MBP.gateway>
2018-09-25 02:21:16 +1000

    comment

--- COMP9041/ass/ass1/legit.pl
+++ COMP9041/ass/ass1/legit.pl
@@ -3,8 +3,8 @@
 use File::Copy "cp";
 use File::Compare;
 
-$legit_dir = ".legit";					# create a directory named .legit
-$index_path = "$legit_dir/index";		# index path in repo
+$legit_dir = ".legit";					      # create repo .legit
+$index_path = "$legit_dir/index";		  # index path in repo
 $legit_log = "$legit_dir/.log.txt";		# legit log file
 @commands_hint = "Usage: legit.pl <command> [<args>]\n
 These are the legit commands:
@@ -25,13 +25,13 @@ main();		# execute the main funciton
 sub init {
 	if ($#ARGV != 0) {					# terminate if init command follows with anything
 		print "usage: legit.pl init\n";
-		exit 1;					
-	}
-	if (-d "$legit_dir") {				# terminate if the dir already exists	
-		print "legit.pl: error: .legit already exists\n";	
 		exit 1;
 	}
-	mkdir "$legit_dir" or die;			# create repo
+	if (-d "$legit_dir") {			# terminate if the dir already exists
+		print "legit.pl: error: .legit already exists\n";
+		exit 1;
+	}
+	mkdir "$legit_dir" or die;	# create repo
 	print "Initialized empty legit repository in $legit_dir\n";
 }
 
@@ -39,53 +39,53 @@ sub add {
 	my @files = @_;
 	foreach my $file (@files) {
 		# terminate if ordinary file is not start with an alphanumeric character or contains any illegal characters
-		if ("$file" !~ /^[a-zA-Z0-9]/ || "$file" =~ /[^._a-zA-Z0-9-]/) {	
+		if ("$file" !~ /^[a-zA-Z0-9]/ || "$file" =~ /[^._a-zA-Z0-9-]/) {
 			print "legit.pl: error: invalid filename '$file'\n";
 			exit 1;
-		}	
+		}
 		if (!-e "$file" && !-e "$index_path/$file") {				# terminate if added file doesn't exist
 			print "legit.pl: error: can not open '$file'\n";
-			exit 1;	
+			exit 1;
 		}
 	}
 	if (!-d $index_path) {
-		mkdir "$index_path" or die;		# create index dir in repo if it doesn't exist
+		mkdir "$index_path" or die;		    # create index dir in repo if it doesn't exist
 	}
 	foreach my $file (@files) {
 		if (-e "$index_path/$file" && !-e "$file") {
-			unlink "$index_path/$file";
+			unlink "$index_path/$file";     # delete file in index but not in local dict
 		}
-		cp "$file", $index_path;		# copy files to index dir
+		cp "$file", $index_path;		      # copy files to index dir
 	}
 }
 
 sub commit {
 	my $error_msg = "usage: legit.pl commit [-a] -m commit-message";
-	my @commit_commands = @_;			# get parameters after "./legit.pl commit"
+	my @commit_commands = @_;			      # get parameters after "./legit.pl commit"
 	my $length_of_commands = @commit_commands;
-	if ($length_of_commands == 0) {		# return error message if nothing follows "commit"
+	if ($length_of_commands == 0) {		  # return error message if nothing follows "commit"
 		print "$error_msg\n";
 		exit 1;
 	}
-	elsif ($length_of_commands == 2 && $commit_commands[0] eq "-m") {					
+	elsif ($length_of_commands == 2 && $commit_commands[0] eq "-m") {
 		commit_file($commit_commands[1]);			# pass commit message to commit_file function in "commit -m case"
-	} 
+	}
 	elsif ($length_of_commands == 3 && $commit_commands[0] eq "-a" && $commit_commands[1] eq "-m") {
 		my @index_files = glob "$index_path/*";		# get all files already in the index
 		for $index_file (@index_files) {
 			if (-e $index_file) {
 				my @name_without_path = split('/', $index_file);	# get file name after the last slash for using add() function
 				@name_without_path = $name_without_path[-1];
-				if (-e "@name_without_path") {		
+				if (-e "@name_without_path") {
 					add (@name_without_path);	# add current file to index if exists
-				} 
+				}
 			}
 			else {
 				unlink "$index_path/$index_file";	# delete the file is it doesn't exist in current dir
 			}
 		}
 		commit_file($commit_commands[2]);			# pass commit message to commit_file function in "commit -a -m case"
-	} 
+	}
 	else {
 		print "$error_msg\n";
 		exit 1;
@@ -120,7 +120,7 @@ sub commit_file {
 	close $file;
 	print "Committed as commit $commit_time\n";
 }
-
+# funciton used to compare index files and last commit files
 sub compare_with_last_commit {
 	my $commit_time = 0;		# set a counter to record dir retrieve times
 	my $commited_folder = "$legit_dir/".".commit";
@@ -128,11 +128,11 @@ sub compare_with_last_commit {
 	my $index_content = "";
 	my $last_commit_content = "";
 	my @index_files = glob "$index_path/*";
-	foreach my $index_file (@index_files) {
+	foreach my $index_file (@index_files) {    # write all files in index as a string
 		open my $file, '<', "$index_file" or die;
 		my @name_without_path = split('/', $index_file);
 		@name_without_path = $name_without_path[-1];
-		$index_content .= "@name_without_path\n";		
+		$index_content .= "@name_without_path\n";
 		while (my $line = <$file>) {
 			$index_content .= $line;
 		}
@@ -142,7 +142,7 @@ sub compare_with_last_commit {
 		$commit_time += 1;
 		$folder = "$commited_folder"."$commit_time";
 	}
-	if ($commit_time > 0) {
+	if ($commit_time > 0) {                    # write all files in last commit as a string
 		my $last_commit_time = $commit_time - 1;
 		my $last_commit_folder = "$commited_folder"."$last_commit_time";
 		my @last_commit_files = glob "$last_commit_folder/*";
@@ -150,13 +150,13 @@ sub compare_with_last_commit {
 			open my $file, '<', "$last_commit_file" or die;
 			my @name_without_path = split('/', $last_commit_file);
 			@name_without_path = $name_without_path[-1];
-			$last_commit_content .= "@name_without_path\n";		
+			$last_commit_content .= "@name_without_path\n";
 			while (my $line = <$file>) {
 				$last_commit_content .= $line;
 			}
 		close $file;
 		}
-		if ($index_content eq $last_commit_content) {
+		if ($index_content eq $last_commit_content) {     # compare two strings
 			return 1;
 		} else {
 			return 0;
@@ -169,7 +169,7 @@ sub show {
 		print "usage: legit.pl <commit>:<filename>\n";
 		exit 1;
 	}
-	my ($commit_time, $filename) = split(/:/, $ARGV[1]);	# split <commit_time>:<$filename> 
+	my ($commit_time, $filename) = split(/:/, $ARGV[1]);	# split <commit_time>:<$filename>
 	if ("$filename" !~ /^[a-zA-Z0-9]/ || "$filename" =~ /[^._a-zA-Z0-9-]/) {
 		print "legit.pl: error: invalid filename '$filename'\n";
 		exit 1;
@@ -181,7 +181,7 @@ sub show {
 		}
 		if (!-e "$legit_log") {			# repo has no commits if legit_log is not created
 			print "legit.pl: error: your repository does not have any commits yet\n";
-			exit 1;		
+			exit 1;
 		}
 		open my $file, '<', "$index_path/$filename";	# print the file if it exists in index
 		foreach $line (<$file>)	{
@@ -225,7 +225,7 @@ sub _log {
 	close $file;
 	foreach my $line (reverse @log_content) {		# print legit log in reversed sequence
 		print "$line";
-	}	
+	}
 }
 
 sub rm {
@@ -280,30 +280,30 @@ sub rm {
 		foreach my $filename (@rm_command) {
 			if (!-e "$index_path/$filename") {
 					print "legit.pl: error: '$filename' is not in the legit repository\n";
-					exit 1;
+					exit 1;    # file not exists in index
 			}
 			elsif (compare("$filename", "$index_path/$filename") != 0 && compare("$index_path/$filename", "$last_commit_folder/$filename") != 0) {
 				print "legit.pl: error: '$filename' in index is different to both working file and repository\n";
-				exit 1;
+				exit 1;      # files in index and local and last commit are different
 			}
 			elsif (compare("$filename", "$index_path/$filename") != 0) {
 				print "legit.pl: error: '$filename' in repository is different to working file\n";
-				exit 1;
+				exit 1;      # files in index and local are different
 			}
 			elsif (compare("$index_path/$filename", "$last_commit_folder/$filename") != 0) {
 				print "legit.pl: error: '$filename' has changes staged in the index\n";
-				exit 1;			
+				exit 1;      # files in index and last commit are different
 			}
 			elsif (compare_with_last_commit() == 0) {
 				print "legit.pl: error: '$filename' has changes staged in the index\n";
-				exit 1;
+				exit 1;      # index dict is different from last commit
 			}
 			unlink "$index_path/$filename";
 			unlink "$filename";
-		}	
+		}
 	}
 }
-
+# check whether files and commands in rm function are available
 sub rm_check {
 	my $err_msg = "usage: legit.pl rm [--force] [--cached] <filenames>";
 	@rm_command = @_;
@@ -324,32 +324,31 @@ sub main {
 		print "@commands_hint";
 		exit 1;
 	}
-	my $command = $ARGV[0];				# get user's command
-	if ($command eq "init") {	
-		init();							# execute init() function
+	my $command = $ARGV[0];			# get user's command
+	if ($command eq "init") {
+		init();							      # execute init() function
 	}
-	elsif (!-d "$legit_dir") {			# terminate if repo has not been created
+	elsif (!-d "$legit_dir") {	# terminate if repo has not been created
 		print "legit.pl: error: no .legit directory containing legit repository exists\n";
-		exit 1;		
+		exit 1;
 	}
 	elsif ($command eq "add") {
-		add(@ARGV[1..$#ARGV]);			# execute add() function
+		add(@ARGV[1..$#ARGV]);		# execute add() function
 	}
 	elsif ($command eq "commit") {
-		commit(@ARGV[1..$#ARGV]);		# execute commit() function
+		commit(@ARGV[1..$#ARGV]);	# execute commit() function
 	}
 	elsif ($command eq "show") {
-		show();							# execute show() function
+		show();							      # execute show() function
 	}
 	elsif ($command eq "log") {
-		_log();							# execute _log() function
+		_log();							      # execute _log() function
 	}
 	elsif ($command eq "rm") {
-		rm(@ARGV[1..$#ARGV]);							# execute rm() function
+		rm(@ARGV[1..$#ARGV]);			# execute rm() function
 	}
 	#else {								# terminate if command is not valiable
 	#	print "@commands_hint";
 	#	exit 1;
 	#}
 }
-
