0e1f6925d62656e2aceb42fb69d1d9e4ac069f66
Lingxu Meng <lingxu.meng@student.unsw.edu.au>
2019-06-24 16:59:39 +1000

    ass 1 init

--- COMP9313/Assignment/Assignment1/assignment1/src/main/java/assignment1/Assignment1.java
+++ COMP9313/Assignment/Assignment1/assignment1/src/main/java/assignment1/Assignment1.java
@@ -1,5 +1,18 @@
 package assignment1;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 /**
  * 
  * This class solves the problem posed for Assignment1
@@ -8,5 +21,52 @@ package assignment1;
 public class Assignment1 {
   
 // TODO: Write the source code for your solution here.	
+	public static class TokenizerMapper 
+		extends Mapper<Object, Text, Text, IntWritable>{
+		
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context
+                 ) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
 	
+	public static class IntSumReducer
+    	extends Reducer<Text,IntWritable,Text,IntWritable> {
+ 
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+                    Context context
+                    ) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+	    Configuration conf = new Configuration();
+	    Job job = Job.getInstance(conf, "word count");
+	    job.setJarByClass(Assignment1.class);
+	    job.setMapperClass(TokenizerMapper.class);
+	    job.setCombinerClass(IntSumReducer.class);
+	    job.setReducerClass(IntSumReducer.class);
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(IntWritable.class);
+	    FileInputFormat.addInputPath(job, new Path(args[0]));
+	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+//	    FileInputFormat.addInputPath(job, new Path("/import/glass/3/z5147810/Desktop/wordcount2/input.txt"));
+//	    FileOutputFormat.setOutputPath(job, new Path("/import/glass/3/z5147810/Desktop/wordcount2/"));
+	    System.exit(job.waitForCompletion(true) ? 0 : 1);
+	  }
 }
