2282b1e6b97560ee11bb244ac8f55c05683ef811
Lingxu Meng <lingxu.meng@student.unsw.edu.au>
2019-06-26 16:06:36 +1000

    dev-ing

--- COMP9313/Assignment/Assignment1/assignment1/src/main/java/assignment1/Assignment1.java
+++ COMP9313/Assignment/Assignment1/assignment1/src/main/java/assignment1/Assignment1.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -21,66 +22,80 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
  *
  */
 public class Assignment1 {
-  
-// TODO: Write the source code for your solution here.	
-	public static class TokenizerMapper 
-		extends Mapper<Object, Text, Text, IntWritable>{
-		
+
+	// TODO: Write the source code for your solution here.
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
 
-		public void map(Object key, Text value, Context context
-                 ) throws IOException, InterruptedException {
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			StringTokenizer itr = new StringTokenizer(value.toString());
-			System.out.println(itr.countTokens());
+			/*
+			 * Firstly, put all stringTokenizer object elements into a new ArrayList for the
+			 * 'sliding' part.
+			 */
 			List<String> tokens = new ArrayList<String>();
 			int tokensCount = itr.countTokens();
-			int i = 0;
+			int index = 0;
+			int len = 2;
 			while (itr.hasMoreTokens()) {
 				tokens.add(itr.nextToken());
 			}
-			for (String token: tokens) {
-				System.out.println(token);
-				word.set(token);
+			// slide the ArrayList for getting n-gram phrase
+			while (index <= tokensCount - len) {
+				String sb = "";
+				List<String> sa = new ArrayList<String>();
+				for (int i = index; i < index + len; i++) {
+					sa.add(tokens.get(i));
+				}
+				sb = String.join(" ", sa);
+				word.set(sb);
 				context.write(word, one);
+				index++;
 			}
-			// while (itr.hasMoreTokens()) {
-			// 	word.set(itr.nextToken());
-			// 	context.write(word, one);
-			// }
 		}
 	}
-	
-	public static class IntSumReducer
-    	extends Reducer<Text,IntWritable,Text,IntWritable> {
- 
+
+	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
 		private IntWritable result = new IntWritable();
 
-		public void reduce(Text key, Iterable<IntWritable> values,
-                    Context context
-                    ) throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
 			int sum = 0;
 			for (IntWritable val : values) {
 				sum += val.get();
 			}
+			// System.out.println("sum" + sum);
 			result.set(sum);
+			// System.out.println("key" + key.toString());
+			// System.out.println("result" + result);
 			context.write(key, result);
 		}
 	}
-	
+
 	public static void main(String[] args) throws Exception {
-	    Configuration conf = new Configuration();
-	    Job job = Job.getInstance(conf, "word count");
-	    job.setJarByClass(Assignment1.class);
-	    job.setMapperClass(TokenizerMapper.class);
-	    job.setCombinerClass(IntSumReducer.class);
-	    job.setReducerClass(IntSumReducer.class);
-	    job.setOutputKeyClass(Text.class);
-	    job.setOutputValueClass(IntWritable.class);
-	    FileInputFormat.addInputPath(job, new Path(args[0]));
-	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-//	    FileInputFormat.addInputPath(job, new Path("/import/glass/3/z5147810/Desktop/wordcount2/input.txt"));
-//	    FileOutputFormat.setOutputPath(job, new Path("/import/glass/3/z5147810/Desktop/wordcount2/"));
-	    System.exit(job.waitForCompletion(true) ? 0 : 1);
-	  }
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "word count");
+		String source = args[0];
+		String dest = args[1];
+		FileSystem fs = FileSystem.get(conf);
+		Path in =new Path(source);
+        Path out =new Path(dest);
+        if (fs.exists(out)) {
+            fs.delete(out, true);
+        }
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setJarByClass(Assignment1.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, in);
+		FileOutputFormat.setOutputPath(job, out);
+		// FileInputFormat.addInputPath(job, new Path("/import/glass/3/z5147810/Desktop/PGlife/COMP9313/Assignment/Assignment1/input"));
+		// FileOutputFormat.setOutputPath(job, new Path("output"));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
